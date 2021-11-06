7c6455a579bd3be22b42f5b2de4939dc1e3f5eab
EqualMa <equalma@outlook.com>
2021-11-06 03:56:44 +0000

    chore: add tests

--- src/global/forgotten_impl.rs
+++ src/global/forgotten_impl.rs
@@ -216,3 +216,540 @@ pub fn try_take<T: Any>(k: &SharedForgottenKey<T>) -> Option<Rc<T>> {
         fg.try_take(k)
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use std::cell::RefCell;
+
+    #[test]
+    fn test_drop_key_1() {
+        thread_local! {
+            static DROPPED: RefCell<Vec<String>> = RefCell::new(vec![]);
+        }
+
+        struct MyValue {
+            v: String,
+        }
+
+        impl Drop for MyValue {
+            fn drop(&mut self) {
+                let v = self.v.clone();
+                DROPPED.with(|cell| {
+                    cell.borrow_mut().push(v);
+                })
+            }
+        }
+
+        let v = MyValue {
+            v: "some value".to_string(),
+        };
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (0, 0)
+        );
+
+        let k = super::forget(v);
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 1)
+        );
+
+        assert_eq!(*k.as_usize(), 1);
+
+        let rc = super::get(&k);
+
+        assert_eq!(rc.v, "some value");
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 1)
+        );
+
+        drop(k);
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 0)
+        );
+
+        DROPPED.with(|cell| {
+            assert_eq!(cell.borrow().len(), 0);
+        });
+
+        drop(rc);
+
+        DROPPED.with(|cell| {
+            assert_eq!(*cell.borrow(), ["some value"]);
+        });
+    }
+
+    #[test]
+    fn test_drop_key_1_1() {
+        test_drop_key_1();
+
+        thread_local! {
+            static DROPPED: RefCell<Vec<String>> = RefCell::new(vec![]);
+        }
+
+        struct MyValue {
+            v: String,
+        }
+
+        impl Drop for MyValue {
+            fn drop(&mut self) {
+                let v = self.v.clone();
+                DROPPED.with(|cell| {
+                    cell.borrow_mut().push(v);
+                })
+            }
+        }
+
+        let v = MyValue {
+            v: "some value 2".to_string(),
+        };
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 0)
+        );
+
+        let k = super::forget(v);
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (2, 1)
+        );
+
+        assert_eq!(*k.as_usize(), 2);
+
+        let rc = super::get(&k);
+
+        assert_eq!(rc.v, "some value 2");
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (2, 1)
+        );
+
+        drop(k);
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (2, 0)
+        );
+
+        DROPPED.with(|cell| {
+            assert_eq!(cell.borrow().len(), 0);
+        });
+
+        drop(rc);
+
+        DROPPED.with(|cell| {
+            assert_eq!(*cell.borrow(), ["some value 2"]);
+        });
+    }
+
+    #[test]
+    fn test_drop_key_2() {
+        thread_local! {
+            static DROPPED: RefCell<Vec<String>> = RefCell::new(vec![]);
+        }
+
+        struct MyValue {
+            v: String,
+        }
+
+        impl Drop for MyValue {
+            fn drop(&mut self) {
+                let v = self.v.clone();
+                DROPPED.with(|cell| {
+                    cell.borrow_mut().push(v);
+                })
+            }
+        }
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (0, 0)
+        );
+
+        let v = MyValue {
+            v: "some value".to_string(),
+        };
+
+        let k = super::forget(v);
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 1)
+        );
+
+        assert_eq!(*k.as_usize(), 1);
+
+        let rc = super::get(&k);
+
+        assert_eq!(rc.v, "some value");
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 1)
+        );
+
+        let v2 = MyValue {
+            v: "some value 2".to_string(),
+        };
+
+        let k2 = super::forget(v2);
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (2, 2)
+        );
+
+        assert_eq!(*k2.as_usize(), 2);
+
+        let rc2 = super::get(&k2);
+
+        assert_eq!(rc2.v, "some value 2");
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (2, 2)
+        );
+
+        drop(k);
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (2, 1)
+        );
+
+        {
+            assert_eq!(*k2.as_usize(), 2);
+
+            let rc2 = super::get(&k2);
+
+            assert_eq!(rc2.v, "some value 2");
+
+            assert_eq!(
+                super::FORGOTTEN.with(|cell| {
+                    let fg = cell.borrow();
+                    (fg.cur, fg.map.len())
+                }),
+                (2, 1)
+            );
+        }
+
+        drop(k2);
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (2, 0)
+        );
+
+        DROPPED.with(|cell| {
+            assert_eq!(cell.borrow().len(), 0);
+        });
+
+        drop(rc2);
+
+        DROPPED.with(|cell| {
+            assert_eq!(*cell.borrow(), ["some value 2"]);
+        });
+
+        drop(rc);
+
+        DROPPED.with(|cell| {
+            assert_eq!(*cell.borrow(), ["some value 2", "some value"]);
+        });
+    }
+
+    #[test]
+    fn test_shared_key() {
+        thread_local! {
+            static DROPPED: RefCell<Vec<String>> = RefCell::new(vec![]);
+        }
+
+        struct MyValue {
+            v: String,
+        }
+
+        impl Drop for MyValue {
+            fn drop(&mut self) {
+                let v = self.v.clone();
+                DROPPED.with(|cell| {
+                    cell.borrow_mut().push(v);
+                })
+            }
+        }
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (0, 0)
+        );
+
+        let v = MyValue {
+            v: "some value".to_string(),
+        };
+
+        let k = super::forget(v);
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 1)
+        );
+
+        assert_eq!(*k.as_usize(), 1);
+
+        let k = k.into_shared();
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 1)
+        );
+
+        {
+            let rc = super::try_get(&k);
+
+            assert!(if let Some(v) = rc {
+                v.v == "some value"
+            } else {
+                false
+            });
+        }
+
+        let k1 = k.clone();
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 1)
+        );
+
+        drop(k);
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 1)
+        );
+
+        {
+            let rc1 = super::try_get(&k1);
+
+            DROPPED.with(|cell| {
+                assert_eq!(cell.borrow().len(), 0);
+            });
+
+            assert!(if let Some(v) = &rc1 {
+                v.v == "some value"
+            } else {
+                false
+            });
+
+            DROPPED.with(|cell| {
+                assert_eq!(cell.borrow().len(), 0);
+            });
+
+            assert!(super::try_free(&k1));
+
+            assert_eq!(
+                super::FORGOTTEN.with(|cell| {
+                    let fg = cell.borrow();
+                    (fg.cur, fg.map.len())
+                }),
+                (1, 0)
+            );
+
+            assert!(super::try_get(&k1).is_none());
+            assert!(!super::try_free(&k1));
+
+            assert_eq!(
+                super::FORGOTTEN.with(|cell| {
+                    let fg = cell.borrow();
+                    (fg.cur, fg.map.len())
+                }),
+                (1, 0)
+            );
+
+            DROPPED.with(|cell| {
+                assert_eq!(cell.borrow().len(), 0);
+            });
+        }
+
+        DROPPED.with(|cell| {
+            assert_eq!(*cell.borrow(), ["some value"]);
+        });
+    }
+
+    #[test]
+    fn test_dropped_shared_key() {
+        thread_local! {
+            static DROPPED: RefCell<Vec<String>> = RefCell::new(vec![]);
+        }
+
+        struct MyValue {
+            v: String,
+        }
+
+        impl Drop for MyValue {
+            fn drop(&mut self) {
+                let v = self.v.clone();
+                DROPPED.with(|cell| {
+                    cell.borrow_mut().push(v);
+                })
+            }
+        }
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (0, 0)
+        );
+
+        {
+            let v = MyValue {
+                v: "some value".to_string(),
+            };
+
+            let k = super::forget(v);
+
+            assert_eq!(
+                super::FORGOTTEN.with(|cell| {
+                    let fg = cell.borrow();
+                    (fg.cur, fg.map.len())
+                }),
+                (1, 1)
+            );
+
+            assert_eq!(*k.as_usize(), 1);
+
+            k.into_shared();
+        }
+
+        DROPPED.with(|cell| {
+            assert_eq!(cell.borrow().len(), 0);
+        });
+
+        assert_eq!(
+            super::FORGOTTEN.with(|cell| {
+                let fg = cell.borrow();
+                (fg.cur, fg.map.len())
+            }),
+            (1, 1)
+        );
+
+        {
+            let k1 = unsafe { crate::SharedForgottenKey::<MyValue>::from_usize(1) };
+            let rc1 = super::try_get(&k1);
+
+            assert!(if let Some(v) = &rc1 {
+                v.v == "some value"
+            } else {
+                false
+            });
+
+            assert_eq!(
+                super::FORGOTTEN.with(|cell| {
+                    let fg = cell.borrow();
+                    (fg.cur, fg.map.len())
+                }),
+                (1, 1)
+            );
+            DROPPED.with(|cell| {
+                assert_eq!(cell.borrow().len(), 0);
+            });
+
+            assert!(super::try_free(&k1));
+
+            assert_eq!(
+                super::FORGOTTEN.with(|cell| {
+                    let fg = cell.borrow();
+                    (fg.cur, fg.map.len())
+                }),
+                (1, 0)
+            );
+
+            assert!(super::try_get(&k1).is_none());
+            assert!(!super::try_free(&k1));
+
+            assert_eq!(
+                super::FORGOTTEN.with(|cell| {
+                    let fg = cell.borrow();
+                    (fg.cur, fg.map.len())
+                }),
+                (1, 0)
+            );
+
+            DROPPED.with(|cell| {
+                assert_eq!(cell.borrow().len(), 0);
+            });
+        }
+
+        DROPPED.with(|cell| {
+            assert_eq!(*cell.borrow(), ["some value"]);
+        });
+    }
+}
