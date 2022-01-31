9adf763c9fd334eefdec2d5449049b78e0942d38
EqualMa <equalma@outlook.com>
2022-01-31 13:41:06 +0800

    feat: support non-sized type

--- src/global/key.rs
+++ src/global/key.rs
@@ -6,9 +6,9 @@ use std::{
 
 use super::SharedForgottenKey;
 
-pub struct ForgottenKey<T: Any>(usize, PhantomData<T>);
+pub struct ForgottenKey<T: ?Sized + Any>(usize, PhantomData<T>);
 
-impl<T: Any> ForgottenKey<T> {
+impl<T: ?Sized + Any> ForgottenKey<T> {
     #[inline]
     pub(super) fn take_usize(&mut self) -> usize {
         std::mem::replace(&mut self.0, 0)
@@ -30,7 +30,7 @@ impl<T: Any> ForgottenKey<T> {
     }
 }
 
-impl<T: Any> Debug for ForgottenKey<T> {
+impl<T: ?Sized + Any> Debug for ForgottenKey<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_tuple(format!("ForgottenKey<{:?}>", TypeId::of::<T>()).as_str())
             .field(&self.0)
@@ -38,7 +38,7 @@ impl<T: Any> Debug for ForgottenKey<T> {
     }
 }
 
-impl<T: Any> Drop for ForgottenKey<T> {
+impl<T: ?Sized + Any> Drop for ForgottenKey<T> {
     fn drop(&mut self) {
         if self.0 != 0 {
             unsafe { super::try_free_with_usize(self.0) };
